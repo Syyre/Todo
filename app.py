e9968d265395e7685f9ff201461e0f3a3aef0c98
Syyre <z5419643@ad.unsw.edu.au>
2024-07-02 11:37:57 +1000

    added extra functions for delete,edit and check.

--- app.py
+++ app.py
@@ -1,19 +1,44 @@
 from flask import Flask, render_template, redirect, request, url_for
 
+#TODO: add css to style web
+
 app = Flask(__name__, template_folder="templates")
 
 """
     todo List in form {task:"blah", done: bool}
 """
-todos = [{"todo":"Sample Todo", "done":False}]
+todos = []
 
 @app.route("/")
 def index():
     return render_template("index.html", todos=todos)
 
-#TODO: Add more functionality. Ability to create more tasks, edit/delete tasks.
-#TODO: edit index.html files with css.
 
+@app.route("/add", methods=["POST"])
+def add():
+    todo = request.form['todo'].strip()
+    if todo:
+        todos.append({"task":todo, "done": False})
+    return redirect(url_for("index"))    
+
+@app.route("/edit/<int:index>", methods=["POST", "GET"])
+def edit(index):
+    todo = todos[index]
+    if request.method == "POST":
+        todo['task'] = request.form["todo"]
+        return redirect(url_for("index"))
+    else:
+        return render_template("edit.html", todo=todo, index=index)
+
+@app.route("/check/<int:index>", methods=['POST'])
+def check(index):
+    todos[index]['done'] = not todos[index]['done']
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:index>", methods=['POST'])
+def delete(index):
+    del todos[index]
+    return redirect(url_for("index"))
 
 if __name__ == '__main__':
     app.run(debug=True)
